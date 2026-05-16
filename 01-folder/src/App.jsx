de3ba1c1b643060e91babe23e3e20d0d42ff3231
ssmp10 <ssmpamj10@gmail.com>
2026-05-17 01:35:53 +0530

    01-folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+     <div id='dad'>
+      <h1 id='child1'>Hello</h1>
+      
+     </div>
+    </>
+  )
+}
+
+export default App
